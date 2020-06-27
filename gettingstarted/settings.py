fd3c9996cd9e733ce7888e700acb0a1f62c58653
Daniel Chang <danielchang083@gmail.com>
2020-06-26 21:24:26 -0700

    still

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -115,5 +115,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'hello/templates')
+    ]
 django_heroku.settings(locals())
