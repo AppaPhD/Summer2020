17cd2ada4e023c3eebef9b3b248c9904fd049d30
Samuel <leumas.uil@gmail.com>
2020-06-28 16:52:38 -0700

    konva commit
    Signed-off-by: Samuel <leumas.uil@gmail.com>

--- hello/views.py
+++ hello/views.py
@@ -8,7 +8,7 @@ import os
 # Create your views here.
 def index(request):
     #return HttpResponse('Hello from Python!')
-    return render(request, "draggableJS.html")
+    return render(request, "konvalab.html")
     #r = requests.get('http://httpbin.org/status/418')
     #print(r.text)
     #return HttpResponse('<pre>' + r.text + '</pre>')
